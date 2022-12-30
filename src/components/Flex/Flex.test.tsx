5609f289af1abe4d713a6314e20ac07fef5020a7
rezonmain <adiazgomez101@gmail.com>
2022-12-29 20:04:41 -0800

    repo: config jest

--- src/components/Flex/Flex.test.tsx
+++ src/components/Flex/Flex.test.tsx
@@ -1,11 +1,11 @@
 import React from "react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
 import Flex from "./Flex";
 
 describe("<Flex/> Component", () => {
-  it("Renders as default", async () => {
-    const container = render(<Flex>hello</Flex>);
-    expect(container).toMatchSnapshot();
+  it("Renders children", async () => {
+    render(<Flex>hello</Flex>);
+    expect(await screen.findByText("hello")).toBeInTheDocument();
   });
 });
